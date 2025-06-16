47f29d36e9c0d1a9267f7501059199dfcfcd04f2
co1seam <ginshy@yandex.ru>
2025-06-16 22:42:03 +0300

    fix: update FileChunk

--- gen/go/media/media.pb.go
+++ gen/go/media/media.pb.go
@@ -602,7 +602,6 @@ type FileResponse struct {
 	state         protoimpl.MessageState `protogen:"open.v1"`
 	FileId        string                 `protobuf:"bytes,1,opt,name=file_id,json=fileId,proto3" json:"file_id,omitempty"`
 	Url           string                 `protobuf:"bytes,2,opt,name=url,proto3" json:"url,omitempty"`
-	OwnerId       string                 `protobuf:"bytes,3,opt,name=owner_id,json=ownerId,proto3" json:"owner_id,omitempty"`
 	unknownFields protoimpl.UnknownFields
 	sizeCache     protoimpl.SizeCache
 }
@@ -651,13 +650,6 @@ func (x *FileResponse) GetUrl() string {
 	return ""
 }
 
-func (x *FileResponse) GetOwnerId() string {
-	if x != nil {
-		return x.OwnerId
-	}
-	return ""
-}
-
 var File_media_media_proto protoreflect.FileDescriptor
 
 const file_media_media_proto_rawDesc = "" +
@@ -701,11 +693,10 @@ const file_media_media_proto_rawDesc = "" +
 	"\bis_first\x18\x04 \x01(\bR\aisFirst\x12\x1b\n" +
 	"\tfile_name\x18\x05 \x01(\tR\bfileName\"&\n" +
 	"\vFileRequest\x12\x17\n" +
-	"\afile_id\x18\x01 \x01(\tR\x06fileId\"T\n" +
+	"\afile_id\x18\x01 \x01(\tR\x06fileId\"9\n" +
 	"\fFileResponse\x12\x17\n" +
 	"\afile_id\x18\x01 \x01(\tR\x06fileId\x12\x10\n" +
-	"\x03url\x18\x02 \x01(\tR\x03url\x12\x19\n" +
-	"\bowner_id\x18\x03 \x01(\tR\aownerId2\xe0\x03\n" +
+	"\x03url\x18\x02 \x01(\tR\x03url2\xe0\x03\n" +
 	"\fMediaService\x12D\n" +
 	"\vCreateMedia\x12\x1c.media.v1.CreateMediaRequest\x1a\x17.media.v1.MediaResponse\x12>\n" +
 	"\bGetMedia\x12\x19.media.v1.GetMediaRequest\x1a\x17.media.v1.MediaResponse\x12D\n" +
